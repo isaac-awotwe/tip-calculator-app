9f1fd6934ca9282be1b9a997566fe77c9f1ec025
Isaac Awotwe <isaacawotwe@gmail.com>
2024-03-12 14:09:27 -0600

    Add the tip_calculator python script

---
+++ tip_calculator.py
@@ -0,0 +1,16 @@
+''' 
+This application calculates the portion of bill each person pays when  bill is distributed equally among a group.
+'''
+# Create a greeting for your program.
+print("Welcome to the Tip Calculator!")
+
+# Ask the user for the total bill amount
+bill = input("What was the total bill?\n$ ")
+# Ask the user for the tip percentage they like to give
+tip = input("How much tip would you like to give? (Enter a percentage amount without the '%' sign)\n")
+# Ask the uder for the number of people to split the bill
+num_people = input("How many people to split the bill?\n")
+
+# Calculate total bill per person
+bill_per_person = round((bill*(1+tip/100))/num_people, 2)
+print(f"Each person should pay: $ {bill_per_person}")
