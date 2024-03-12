b17bcc9d3708b962526800765cffc87d0fa994b8
Isaac Awotwe <isaacawotwe@gmail.com>
2024-03-12 17:11:41 -0600

    Update streamlit code script

--- tip_calc_st.py
+++ tip_calc_st.py
@@ -21,32 +21,32 @@ if st.session_state.stage >= 1:
 
 if st.session_state.stage >=2:
     st.write(f'Hello {name.capitalize()}!')
-    bill = float(st.text_input(
-        "What is the total bill? (Enter amount in dollars but without the $ sign. e.g. write 150.20 for $150.20)",
+    bill = st.text_input(
+        "What is the total bill? (Enter amount in dollars but without the dollar sign)",
         on_change = set_state, args=[3]
-        ))
-    st.write(f"You have entered a bill of $ {bill:.2f}")
+        )
 
     if bill is None:
         set_state(2)
 
 if st.session_state.stage >=3:
-    tip = float(st.text_input(
+    tip = st.text_input(
         "How much tip would you like to give? (Enter a percentage amount without the '%' sign. E.g. write 10 for 10%)",
-        on_change = set_state, args = [4]))
-    st.write(f"You have entered a tip of {tip:.2f}%")
+        on_change = set_state, args = [4])
+    
     if tip is None:
         set_state(3)
 
 
 if st.session_state.stage >=4:
-    num_people = int(st.text_input(
+    num_people = st.text_input(
         "How many people are to split the bill?",
-        on_change = set_state, args = [5]))
-    if tip is None:
+        on_change = set_state, args = [5])
+
+    if num_people is None:
         set_state(4)
 
 if st.session_state.stage >=5:
-    bill_per_person = (bill*(1+tip/100))/num_people
-    st.markdown(f"Each person should pay: $ {bill_per_person:.2f}")
+    bill_per_person = (float(bill)*(1+float(tip)/100))/int(num_people)
+    st.markdown(f"##### Each person should pay:  :orange[$ {bill_per_person:.2f}]")
     st.button("Start Over", on_click=set_state, args=[0])
