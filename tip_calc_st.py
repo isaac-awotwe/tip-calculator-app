99f6121da2f6c288ff1107896f04e68637dde695
Isaac Awotwe <isaacawotwe@gmail.com>
2024-03-13 15:14:56 -0600

    Add a dollar sign to bill amount user prompt for streamlit script

--- tip_calc_st.py
+++ tip_calc_st.py
@@ -22,7 +22,7 @@ if st.session_state.stage >= 1:
 if st.session_state.stage >=2:
     st.write(f'Hello {name.capitalize()}!')
     bill = st.text_input(
-        "What is the total bill? (Enter amount in dollars but without the dollar sign. For example write 150.25 for $150.25)",
+        "What is the total bill? $",
         on_change = set_state, args=[3]
         )
 
